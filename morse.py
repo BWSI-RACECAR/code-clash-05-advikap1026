a2f1f9bd35d8d6d4f9a4d56eb03462cef03029a7
advikap1026 <127911640+advikap1026@users.noreply.github.com>
2023-07-18 09:06:05 -0400

    Add files via upload

---
+++ morse.py
@@ -0,0 +1,38 @@
+class Solution:
+    def encrypt(self, message):
+            #type message: string
+            #return type: string
+            
+            #TODO: Write code below to return a string with the solution to the prompt.
+            MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', '.':'.-.-.-', '?':'..--..', '!':'-.-.--', 
+                    '/':'-..-.', '-':'-....-', '(':'-.--.', ')':'-.--.-'}
+            newString = ""
+            for i in range(len(message)):
+                if message[i] == " ":
+                    newString += "  "
+                    continue 
+                newString += MORSE_CODE_DICT.get(message[i]) + " "
+
+            return newString 
+            pass
+
+def main():
+     str1=input()
+     tc1= Solution()
+     ans=tc1.encrypt(str1)
+     print(ans)
+
+if __name__ == '__main__':
+    main()
